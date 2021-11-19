3348d56de0ec674722917ccfaae223b09e626dc0
ryanzola <ryanzola@me.com>
2021-11-19 00:40:40 -0500

    :hammer_and_wrench: optimized texture loading

--- src/classes/Room.js
+++ src/classes/Room.js
@@ -21,7 +21,7 @@ export default class Room {
     this.textures.wall = this.textureLoader.load('/assets/textures/walls.png')
     this.textures.brick = this.textureLoader.load('/assets/textures/brick.png')
     this.textures.floor = this.textureLoader.load('/assets/textures/floor.png')
-    this.textures.clouds = this.textureLoader.load('/assets/textures/clouds.png')
+    this.textures.cloud = this.textureLoader.load('/assets/textures/clouds.png')
     this.textures.pipe = this.textureLoader.load('/assets/textures/pipe.png')
     this.textures.cubes = this.textureLoader.load('/assets/textures/cubes.png')
 
@@ -34,53 +34,21 @@ export default class Room {
   setMaterials() {
     this.materials = {}
 
-    this.materials.wall = new THREE.MeshBasicMaterial({
-      map: this.textures.wall
-    })
-
-    this.materials.brick = new THREE.MeshBasicMaterial({
-      map: this.textures.brick
-    })
-
-    this.materials.floor = new THREE.MeshBasicMaterial({
-      map: this.textures.floor
-    })
-
-    this.materials.cloud = new THREE.MeshBasicMaterial({
-      map: this.textures.clouds
-    })
-
-    this.materials.pipe = new THREE.MeshBasicMaterial({
-      map: this.textures.pipe
-    })
-
-    this.materials.cubes = new THREE.MeshBasicMaterial({
-      map: this.textures.cubes
-    })
+    for(const texture in this.textures) {
+      this.materials[texture] = new THREE.MeshBasicMaterial({
+        map: this.textures[texture]
+      })
+    }
   }
 
   setModel() {
     this.modelLoader.load('/assets/models/walls.glb', glb => {
       this.scene.add(glb.scene)
-      // console.log(glb.scene)
 
-      const wallMesh = glb.scene.children.find(child => child.name === 'wall')
-      wallMesh.material = this.materials.wall
-
-      const brickMesh = glb.scene.children.find(child => child.name === 'brick')
-      brickMesh.material = this.materials.brick
-
-      const floorMesh = glb.scene.children.find(child => child.name === 'floor')
-      floorMesh.material = this.materials.floor
-
-      const cloudMesh = glb.scene.children.find(child => child.name === 'cloud')
-      cloudMesh.material = this.materials.cloud
-
-      const pipeMesh = glb.scene.children.find(child => child.name === 'pipe')
-      pipeMesh.material = this.materials.pipe
-
-      const cubesMesh = glb.scene.children.find(child => child.name === 'cubes')
-      cubesMesh.material = this.materials.cubes
+      for(const material in this.materials) {
+        const mesh = glb.scene.children.find(child => child.name === material)
+        mesh.material = this.materials[material]
+      }
     })
   }
 }
